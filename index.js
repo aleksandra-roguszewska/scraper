41532ba2ebbc3df4c0f9002c86a5a7cc16bdedef
Aleksandra Roguszewska <ola.roguszewska@gmail.com>
2023-07-11 14:19:27 +0200

    feat: Films from 4 vods are saved in CSV and sorted by rating

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const PORT = 8000;
+const PORT = 7000;
 const axios = require("axios");
 const cheerio = require("cheerio");
 const express = require("express");
@@ -25,11 +25,13 @@ const netflix = { path: "netflix", name: "NETFLIX" };
 const canalPlus = { path: "canal_plus_manual", name: "CANAL+" };
 const disney = { path: "disney", name: "DISNEY+" };
 
+const vodServicesArray = [HBO, netflix, canalPlus, disney];
+
 const currentYear = "2023"; // Tu jeszcze moge zrobić,żeby ściągało zawsze najnowszy rok
 
 const getFilms = (baseUrl, vodService, year) => {
   const url = `${baseUrl}/${vodService.path}/film/${currentYear}`;
-  axios(url)
+  return axios(url)
     .then((response) => {
       const html = response.data;
       const $ = cheerio.load(html);
@@ -42,17 +44,24 @@ const getFilms = (baseUrl, vodService, year) => {
         films.push({ title, rating, vodServiceName: vodService.name });
         counter++;
       });
-      console.log(films);
-
-      csvWriter
-        .writeRecords(films) // returns a promise
-        .then(() => {
-          console.log("...Done");
-        });
+      return films;
     })
     .catch((err) => console.log(err));
 };
 
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
 
-getFilms(baseUrl, disney, currentYear);
+const promises = vodServicesArray.map((vodService) => {
+  return getFilms(baseUrl, vodService, currentYear);
+});
+
+Promise.all(promises)
+  .then((results) => {
+    const unsortedFilms = results.flat();
+    const films = unsortedFilms.sort((a, b) => b.rating - a.rating);
+    return csvWriter.writeRecords(films);
+  })
+  .then(() => {
+    console.log("Success: Films saved to films.csv file");
+  })
+  .catch((err) => console.log(err));
